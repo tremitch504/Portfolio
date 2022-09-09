7512eb082b8de118af2b5f1661a73e8ca50b6412
tremitch504 <trevon@students.alcorn.edu>
2022-09-09 00:45:51 -0500

    Added a Typlical feature for roles text

--- client/src/PortfolioContainer/HomePage/Profile.js
+++ client/src/PortfolioContainer/HomePage/Profile.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Typical from 'react-typical';
 
 const Profile = () => {
    
-
+//info = details
 return (
     <div className='container-profile'>
         <div className='parent-profile'>
@@ -11,6 +11,9 @@ return (
                 <div className='colz'>
                     <a href='https://github.com/tremitch504'>
                         <span className='bi bi-github'></span>
+                    </a>
+                    <a href='https://www.linkedin.com/in/tre-mitch'>
+                    <i className="bi bi-linkedin"></i>
                     </a>
                     <a href='#'>
                         <i className='fa fa-google-plus-square'></i>
@@ -22,6 +25,36 @@ return (
                         <i className='gitHub-square'></i>
                     </a> */}
                 </div>
+            
+            <div className='info-profile-name'>
+                <span className='primary-text'>
+                    {" "}
+                    Hello, I'm <span className='highLight-text'>Tre'von</span>
+                </span>
+            </div>
+            <div className='profile-info-role'>
+                <span className='primary-text'>
+                    {" "}
+                    <h1>
+                       {" "}
+                       <Typical 
+                       loop={Infinity}
+                       steps={[
+                        "Full Stack Developer 💻",
+                        1000,
+                        "MERN Stack Dev 🖥",
+                        1000,
+                        "React Native Dev 📱",
+                        1000,
+                        "Software Developer 🌐",
+                        1000,
+                        "Software Engineer Dev",
+                        1000,
+                       ]}
+                       />
+                    </h1>
+                </span>
+            </div>
             </div>
         </div>
     </div>
